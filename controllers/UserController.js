670e72127e021dcfd26de1862986d0daf69706a9
titus-mumo <tituskennedy74@gmail.com>
2023-10-02 12:10:49 +0300

    await bcrypt func

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -25,7 +25,7 @@ const getUserByID =  asyncHandler(async(req, res)=>{
 const postUser = async (req, res) => {
     try {
         let {email, password} = req.body;
-        const hashedPassword = bcrypt.hash(password, 10)
+        const hashedPassword = await bcrypt.hash(password, 10)
         const user = await User.create({email, password: hashedPassword})
         res.status(200).json(user)
     } catch (error) {
